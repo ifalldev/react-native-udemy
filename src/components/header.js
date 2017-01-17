b16a1f48a86a816249cecb9470205cc52c9fc576
ifalldev <cbatista.93@gmail.com>
2017-01-17 15:56:41 -0200

    fun & first app with first components

---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+// Import libraries for making a Component
+import React from 'react';
+import { Text } from 'react-native';
+// Make a Component
+const Header = () => <Text>Albums</Text>;
+// Make the Component available to other parts fo the App
+export default Header;
