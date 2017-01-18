fb642d04d5061e80dc2a3b6a54b97f38a3b97961
ifalldev <cbatista.93@gmail.com>
2017-01-18 08:49:52 -0200

    reusable header

--- index.ios.js
+++ index.ios.js
@@ -4,7 +4,7 @@ import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 // create a Component
 const App = () => (
-  <Header />
+  <Header headerText={'Albums'} />
 );
 
 // Render it to the device
