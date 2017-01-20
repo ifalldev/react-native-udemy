148ff952cc2e98b3c3ae10f03e9049767c23116b
ifalldev <cbatista.93@gmail.com>
2017-01-20 19:23:36 -0200

    refac card style; add thumbnail & album cover

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,7 @@ import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 // create a Component
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
